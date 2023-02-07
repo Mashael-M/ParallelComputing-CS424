60d90133a9d72f2bd6c1e84fbb464fd21b2a2ae1
Mashael <108432621+Mashael-M@users.noreply.github.com>
2023-02-07 22:15:44 +0300

    Update Triangular.java

--- Triangular number Series/Triangular.java
+++ Triangular number Series/Triangular.java
@@ -1,44 +1,38 @@
-package triangular;
-
+package triangular_series;
 import java.util.Scanner;
 import java.util.concurrent.*;
 
-public class Triangular {
+public class Triangular_series {
 
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Pleses Enter a positive number: ");
+        //to get the user input
+        int n = input.nextInt();
+        //to create 4 thread
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        //calculate the start time
+        long StartTime = System.currentTimeMillis();
 
-            System.out.println("   Triangular number Series with java using 4 thread   ");
-            System.out.print("- Pleses Enter a positive number: ");
-            //to get the user input
-            int n = input.nextInt(); 
- 
-            
-            //to create 4 thread
-            ExecutorService executor = Executors.newFixedThreadPool(4); 
-            //calculate the start time
-            long StartTime = System.currentTimeMillis();
-
-            // for loop to execute the thread 
-            for (int Thread_Rank = 0; Thread_Rank < 4; Thread_Rank++) {
-                executor.execute(new TriangularNumSeries(Thread_Rank, n)); 
-            }
-
-            executor.shutdown();
-
-            while (!executor.isTerminated()) {
-            } 
-            //calculate the end time
-            long EndTime = System.currentTimeMillis();
-            //calculate the time elapsed
-            System.out.println("The Time elapsed is: " + (EndTime - StartTime) + " milliseconds.");
+        // for loop to execute the thread 
+        for (int i = 0; i < 4; i++) {
+            executor.execute(new TriangularNumSeries(i, n));
         }
-    }
 
+        executor.shutdown();
+
+        while (!executor.isTerminated()) {
+        }
+        //calculate the end time
+        long EndTime = System.currentTimeMillis();
+        //calculate the time elapsed
+        System.out.println("The Time elapsed is: " + (EndTime - StartTime) + " milliseconds.");
+
+    }
 }
 
-//                              THe Triangular number Series Class
 class TriangularNumSeries implements Runnable {
+
     int Thread_Rank;
     int n;
 
@@ -49,9 +43,11 @@ class TriangularNumSeries implements Runnable {
 
     @Override
     public void run() {
-        for (int i = Thread_Rank; i < n; i += 4) {
-            // To print thread rank + iteration + number of dots in the triangular using the fourmela
-            System.out.printf("(Thread:" + Thread_Rank + ") (Itration:" + i + ") (Dots:" + (i * (i + 1)) / 2 + ")\n");
+        int local_m = n / 4;
+        int my_first = local_m * Thread_Rank;
+        int my_last = my_first + local_m;
+        for (int i = my_first; i < my_last; i++) {
+            System.out.println("thread: " + Thread_Rank + " the result is " + i * (i + 1) / 2);
         }
     }
 }
